e2a335b8792d7540426739b8a7430da4d6e27d0d
AngeLopez <angel@angelopez.ca>
2017-04-24 18:43:27 -0400

    project styling

--- assets/index.js
+++ assets/index.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
   $(window).scroll(function () {
     if ($(window).scrollTop() > windowHeight) {
       $('#navbar').addClass('navbar-fixed');
+      $('main').addClass('navbar-fixed-main');
     }
     if ($(window).scrollTop() < windowHeight) {
       $('#navbar').removeClass('navbar-fixed');
+      $('main').removeClass('navbar-fixed-main');
     }
   });
 });
